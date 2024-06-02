bc413f0b40f681ec1163d16ac8e2fb996c7b83e4
kingvpn77 <155173495+king-vpn@users.noreply.github.com>
2024-06-02 20:37:47 +0700

    Update updateyes.sh

--- menu/updateyes.sh
+++ menu/updateyes.sh
@@ -1,93 +1,79 @@
 #!/bin/bash
-clear
-rm -rf upsc.sh
-rm -rf upsc.sh.1
-rm -rf upsc.sh.2
-rm -rf /root/upsc.sh
-rm -rf /root/upsc.sh.1
-rm -rf /root/upsc.sh.2
-dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
-biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
-###########- COLOR CODE -##############
-clear
-echo -e "   \e[41;97;1mUPDATE SCRIPT NEW VERSION\e[0m"
-echo -e ""
-# // Delete data Lama
-cd /usr/bin
-rm -rf menu
-rm -rf dashboard
-rm -rf menu-backup
-rm -rf menu-ssh
-rm -rf menu-trojan
-rm -rf menu-vlesss
-rm -rf menu-vmess
-rm -rf menu-noobzvpns
-rm -rf menu-ss
-rm -rf menu-ipsec
-rm -rf menu-udp
-rm -rf menu-theme
-rm -rf menu-udp
-rm -rf addssh
-rm -rf add-udp
-rm -rf setting
-rm -rf restart
-rm -rf running
-rm -rf system
-rm -rf about
-rm -rf upsc
-rm -rf running
-rm -rf menu-bot
-rm -rf addhost
-rm -rf mbackup
-rm -rf mstrt
-rm -rf usernew
-rm -rf add-udp
-rm -rf addhost
-rm -rf genssl
-rm -rf trial-generator
-rm -rf trial-vmess
-# // Download Data Baru
-# // menu
-wget -q -O /usr/bin/menu "https://raw.githubusercontent.com/king-vpn/seller/master/menu/menu.sh" && chmod +x /usr/bin/menu
-wget -q -O /usr/bin/dashboard "https://raw.githubusercontent.com/king-vpn/seller/master/menu/dashboard.sh" && chmod +x /usr/bin/dashboard
-wget -q -O /usr/bin/menu-ssh "https://raw.githubusercontent.com/king-vpn/seller/master/menu/menu-ssh.sh" &&  chmod +x /usr/bin/menu-ssh
-wget -q -O /usr/bin/menu-trojan "https://raw.githubusercontent.com/king-vpn/seller/master/menu/menu-trojan.sh" && chmod +x /usr/bin/menu-trojan
-wget -q -O /usr/bin/menu-backup "https://raw.githubusercontent.com/king-vpn/seller/master/menu/menu-backup.sh" && chmod +x /usr/bin/menu-backup
-wget -q -O /usr/bin/menu-vless "https://raw.githubusercontent.com/king-vpn/seller/master/menu/menu-vless.sh" && chmod +x /usr/bin/menu-vless
-wget -q -O /usr/bin/menu-vmess "https://raw.githubusercontent.com/king-vpn/seller/master/menu/menu-vmess.sh" && chmod +x /usr/bin/menu-vmess
-wget -q -O /usr/bin/menu-ss "https://raw.githubusercontent.com/king-vpn/seller/master/menu/menu-ss.sh" && chmod +x /usr/bin/menu-ss
-wget -q -O /usr/bin/menu-noobzvpns "https://raw.githubusercontent.com/king-vpn/seller/master/menu/menu-noobzvpns.sh" && chmod +x /usr/bin/menu-noobzvpns
-wget -q -O /usr/bin/menu-udp "https://raw.githubusercontent.com/king-vpn/seller/master/menu/menu-udp.sh" && chmod +x /usr/bin/menu-udp
-wget -q -O /usr/bin/mbackup "https://raw.githubusercontent.com/king-vpn/seller/master/backup/backup.sh" && chmod +x /usr/bin/mbackup
-wget -q -O /usr/bin/menu-bot "https://raw.githubusercontent.com/king-vpn/seller/master/menu/menu-bot.sh" && chmod +x /usr/bin/menu-bot
-wget -q -O /usr/bin/trial-generator "https://raw.githubusercontent.com/king-vpn/seller/master/trial/trial-generator.sh" && chmod +x /usr/bin/trial-generator
-wget -q -O /usr/bin/menu-ipsec "https://raw.githubusercontent.com/king-vpn/seller/master/ipsec/menu-ipsec.sh" && chmod +x /usr/bin/menu-ipsec
-wget -q -O /usr/bin/system "https://raw.githubusercontent.com/king-vpn/seller/master/menu/system.sh" && chmod +x /usr/bin/system
-wget -q -O /usr/bin/about "https://raw.githubusercontent.com/king-vpn/seller/master/menu/about.sh" &&  chmod +x /usr/bin/about
-wget -q -O /usr/bin/running "https://raw.githubusercontent.com/king-vpn/seller/master/menu/running.sh" && chmod +x /usr/bin/running
-wget -q -O /usr/bin/setting "https://raw.githubusercontent.com/king-vpn/seller/master/Themes/setting.sh" && chmod +x /usr/bin/setting
-wget -q -O /usr/bin/upsc "https://raw.githubusercontent.com/king-vpn/seller/master/menu/upsc.sh" && chmod +x /usr/bin/upsc
-wget -q -O /usr/bin/running "https://raw.githubusercontent.com/king-vpn/seller/master/menu/running.sh" && chmod +x /usr/bin/running
-wget -q -O /usr/bin/restart "https://raw.githubusercontent.com/king-vpn/seller/master/menu/restart.sh" && chmod +x /usr/bin/restart
-wget -q -O /usr/bin/addhost  "https://raw.githubusercontent.com/king-vpn/seller/master/addhost.sh" && chmod +x /usr/bin/addhost
-wget -q -O /usr/bin/menu-theme "https://raw.githubusercontent.com/king-vpn/seller/master/Themes/thema.sh" && chmod +x /usr/bin/menu-theme
-
-wget -q -O /usr/bin/trial-generator "https://raw.githubusercontent.com/king-vpn/seller/master/trial/trial-generator.sh" && chmod +x /usr/bin/trial-generator
-wget -q -O /usr/bin/add-udp "https://raw.githubusercontent.com/king-vpn/seller/master/menu/add-udp.sh" && chmod +x /usr/bin/add-udp
-wget -q -O /usr/bin/mstrt "https://raw.githubusercontent.com/king-vpn/seller/master/backup/strt.sh" && chmod +x /usr/bin/mstrt
-wget -q -O /usr/bin/trial-vmess "https://raw.githubusercontent.com/king-vpn/seller/master/trial/trial-vmess.sh" && chmod +x /usr/bin/trial-vmess
-#wget -q -O /usr/bin/menu-bot "https://raw.githubusercontent.com/king-vpn/seller/master/menu/menu-bot.sh" && chmod +x /usr/bin/menu-bot
-wget -q -O /usr/bin/genssl  "https://raw.githubusercontent.com/king-vpn/seller/master/genssl.sh" && chmod +x /usr/bin/genssl
-#wget -q -O /usr/bin/addhost "https://raw.githubusercontent.com/king-vpn/seller/master/menu/addhost.sh" && chmod +x /usr/bin/addhost
-echo -e "sleep 3 "
 
 
+LocalVersion=$(cat /etc/version)
+version=$(curl -sS "https://raw.githubusercontent.com/king-vpn/seller/master/versiupdate")
+
+if [ "$LocalVersion" = "$version" ]; then
+  echo
+else
+  clear
+  figlet -f 3d "gretongers" | lolcat
+  echo "Update tersedia!!" | lolcat
+  echo "Fix Menu" | lolcat
+
+  spinner() {
+    local pid=$1
+    local delay=0.1
+    local spinstr='|/-\'
+    
+    while [ "$(ps a | awk '{print $1}' | grep $pid)" ]; do
+      local temp=${spinstr#?}
+      printf " [%c] " "$spinstr"
+      local spinstr=$temp${spinstr%"$temp"}
+      sleep $delay
+      printf "\b\b\b\b\b\b"
+    done
+    printf "    \b\b\b\b"
+  }
+
+  loading_animation() {
+    local message=$1
+    echo -n "$message"
+    $2 &
+    spinner $!
+  }
+
+  echo "Sedang mengupdate!!!"
+
+  loading_animation "Loading" "sleep 5"
+
+  fun_bar() {
+    CMD[0]="$1"
+    CMD[1]="$2"
+    (
+      [[ -e $HOME/fim ]] && rm $HOME/fim
+      ${CMD[0]} -y >/dev/null 2>&1
+      ${CMD[1]} -y >/dev/null 2>&1
+      touch $HOME/fim
+    ) >/dev/null 2>&1 &
+    tput civis
+    echo -ne "  \033[0;33mPlease Wait Loading \033[1;37m- \033[0;33m["
+    while true; do
+      for ((i = 0; i < 18; i++)); do
+        echo -ne "\033[0;32m#"
+        sleep 0.1s
+      done
+      [[ -e $HOME/fim ]] && rm $HOME/fim && break
+      echo -e "\033[0;33m]"
+      sleep 1s
+      tput cuu1
+      tput dl1
+      echo -ne "  \033[0;33mPlease Wait Loading \033[1;37m- \033[0;33m["
+    done
+    echo -e "\033[0;33m]\033[1;37m -\033[1;32m OK !\033[1;37m"
+    tput cnorm
+  }
+  fun_bar
+
+  # Hapus semua file sebelum mengunduh versi baru
+  rm -f /etc/version
+  rm -f /usr/bin/menu
+  rm -f /usr/bin/menu-ssh
+  # Tambahkan perintah rm untuk file lainnya...
+wget -q -O /etc/version "https://raw.githubusercontent.com/king-vpn/seller/master/versiupdate" && chmod +x /etc/version
+  rm -f /usr/bin/menu
+wget -q -O /usr/bin/menu "https://raw.githubusercontent.com/king-vpn/seller/master/menu.sh" && chmod 777 /usr/bin/menu
 
 
-rm -rf upsc.sh
-echo -e "sleep 3 "
-clear
-echo -e "\e[32;1mSuccessfully\e[0m"
-sleep 2
-clear
-dashboard
+fi
