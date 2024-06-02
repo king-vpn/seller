50013d3a0f59cd9791e6b1c93ecc5724a66aa21b
kingvpn77 <155173495+king-vpn@users.noreply.github.com>
2024-06-02 10:35:14 +0700

    Update bot.sh

--- botssh/bot.sh
+++ botssh/bot.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 MYIP=$(wget -qO- ipinfo.io/ip);
 
-IZIN=$(curl -sS https://raw.githubusercontent.com/myridwan/izinvps/ipuk/ip | awk '{print $4}' | grep $MYIP)
+IZIN=$(curl -sS https://raw.githubusercontent.com/https://raw.githubusercontent.com/king-vpn/seller/master/izin | awk '{print $4}' | grep $MYIP)
 if [ $MYIP = $IZIN ]; then
 echo "IZIN DI TERIMA!!"
 else
@@ -19,7 +19,7 @@ apt install python3 python3-pip -y
 apt install sqlite3 -y
 cd /media/
 rm -rf cybervpn
-wget myrid.digital/drg/botssh/cybervpn.zip
+wget https://raw.githubusercontent.com/king-vpn/seller/master/botssh/cybervpn.zip
 unzip cybervpn.zip
 cd cybervpn
 rm var.txt
@@ -87,86 +87,86 @@ systemctl enable cybervpn
 clear
 echo "downloading asset"
 
-wget -q -O /usr/bin/panelbot "myrid.digital/drg/botssh/panelbot.sh" && chmod +x /usr/bin/panelbot
+wget -q -O /usr/bin/panelbot "https://raw.githubusercontent.com/king-vpn/seller/master/botssh/panelbot.sh" && chmod +x /usr/bin/panelbot
 
 
-wget -q -O /usr/bin/add-ip-bot "myrid.digital/drg/botssh/add-ip-bot.sh" && chmod +x /usr/bin/add-ip-bot
+wget -q -O /usr/bin/add-ip-bot "https://raw.githubusercontent.com/king-vpn/seller/master/botssh/add-ip-bot.sh" && chmod +x /usr/bin/add-ip-bot
 
 
-wget -q -O /usr/bin/addnoobz "myrid.digital/drg/botssh/addnoobz.sh" && chmod +x /usr/bin/addnoobz
-
-
-
-wget -q -O /media/log-install.txt "myrid.digital/drg/botssh/log-install.txt"
+wget -q -O /usr/bin/addnoobz "https://raw.githubusercontent.com/king-vpn/seller/master/botssh/addnoobz.sh" && chmod +x /usr/bin/addnoobz
 
 
 
-wget -q -O /usr/bin/add-vless "myrid.digital/drg/botssh/add-vless.sh" && chmod +x /usr/bin/add-vless
-
-wget -q -O /usr/bin/addtr "myrid.digital/drg/botssh/addtr.sh" && chmod +x /usr/bin/addtr
-
-
-wget -q -O /usr/bin/addws "myrid.digital/drg/botssh/addws.sh" && chmod +x /usr/bin/addws
-
-
-wget -q -O /usr/bin/addss "myrid.digital/drg/botssh/addss.sh" && chmod +x /usr/bin/addss
-
-wget -q -O /usr/bin/cek-ssh "myrid.digital/drg/botssh/cek-ssh.sh" && chmod +x /usr/bin/cek-ssh
+wget -q -O /media/log-install.txt "https://raw.githubusercontent.com/king-vpn/seller/master/botssh/log-install.txt"
 
 
 
-wget -q -O /usr/bin/cek-ss "myrid.digital/drg/botssh/cek-ss.sh" && chmod +x /usr/bin/cek-ss
+wget -q -O /usr/bin/add-vless "https://raw.githubusercontent.com/king-vpn/seller/master/botssh/add-vless.sh" && chmod +x /usr/bin/add-vless
+
+wget -q -O /usr/bin/addtr "https://raw.githubusercontent.com/king-vpn/seller/master/botssh/addtr.sh" && chmod +x /usr/bin/addtr
+
+
+wget -q -O /usr/bin/addws "https://raw.githubusercontent.com/king-vpn/seller/master/botssh/addws.sh" && chmod +x /usr/bin/addws
+
+
+wget -q -O /usr/bin/addss "https://raw.githubusercontent.com/king-vpn/seller/master/botssh/addss.sh" && chmod +x /usr/bin/addss
+
+wget -q -O /usr/bin/cek-ssh "https://raw.githubusercontent.com/king-vpn/seller/master/botssh/cek-ssh.sh" && chmod +x /usr/bin/cek-ssh
 
 
 
-
-wget -q -O /usr/bin/cek-tr "myrid.digital/drg/botssh/cek-tr.sh" && chmod +x /usr/bin/cek-tr
-
-wget -q -O /usr/bin/cek-vless "myrid.digital/drg/botssh/cek-vless.sh" && chmod +x /usr/bin/cek-vless
-
-wget -q -O /usr/bin/cek-ws "myrid.digital/drg/botssh/cek-ws.sh" && chmod +x /usr/bin/cek-ws
-
-wget -q -O /usr/bin/del-vless "myrid.digital/drg/botssh/del-vless.sh" && chmod +x /usr/bin/del-vless
+wget -q -O /usr/bin/cek-ss "https://raw.githubusercontent.com/king-vpn/seller/master/botssh/cek-ss.sh" && chmod +x /usr/bin/cek-ss
 
 
 
 
-wget -q -O /usr/bin/cek-noobz "myrid.digital/drg/botssh/cek-noobz.sh" && chmod +x /usr/bin/cek-noobz
+wget -q -O /usr/bin/cek-tr "https://raw.githubusercontent.com/king-vpn/seller/master/botssh/cek-tr.sh" && chmod +x /usr/bin/cek-tr
+
+wget -q -O /usr/bin/cek-vless "https://raw.githubusercontent.com/king-vpn/seller/master/botssh/cek-vless.sh" && chmod +x /usr/bin/cek-vless
+
+wget -q -O /usr/bin/cek-ws "https://raw.githubusercontent.com/king-vpn/seller/master/botssh/cek-ws.sh" && chmod +x /usr/bin/cek-ws
+
+wget -q -O /usr/bin/del-vless "https://raw.githubusercontent.com/king-vpn/seller/master/botssh/del-vless.sh" && chmod +x /usr/bin/del-vless
 
 
 
-wget -q -O /usr/bin/deltr "myrid.digital/drg/botssh/deltr.sh" && chmod +x /usr/bin/deltr
 
-wget -q -O /usr/bin/delws "myrid.digital/drg/botssh/delws.sh" && chmod +x /usr/bin/delws
-
-
-
-wget -q -O /usr/bin/delss "myrid.digital/drg/botssh/delss.sh" && chmod +x /usr/bin/delss
+wget -q -O /usr/bin/cek-noobz "https://raw.githubusercontent.com/king-vpn/seller/master/botssh/cek-noobz.sh" && chmod +x /usr/bin/cek-noobz
 
 
 
-wget -q -O /usr/bin/renew-ss "myrid.digital/drg/botssh/renew-ss.sh" && chmod +x /usr/bin/renew-ss
+wget -q -O /usr/bin/deltr "https://raw.githubusercontent.com/king-vpn/seller/master/botssh/deltr.sh" && chmod +x /usr/bin/deltr
 
-
-wget -q -O /usr/bin/renewtr "myrid.digital/drg/botssh/renewtr.sh" && chmod +x /usr/bin/renewtr
-
-
-wget -q -O /usr/bin/renewvless "myrid.digital/drg/botssh/renewvless.sh" && chmod +x /usr/bin/renewvless
-
-
-wget -q -O /usr/bin/renewws "myrid.digital/drg/botssh/renewws.sh" && chmod +x /usr/bin/renewws
-
-
-wget -q -O /usr/bin/cek-mws "myrid.digital/drg/botssh/cek-mws.sh" && chmod +x /usr/bin/cek-mws
-
-wget -q -O /usr/bin/cek-mvs "myrid.digital/drg/botssh/cek-mvs.sh" && chmod +x /usr/bin/cek-mvs
-
-
-wget -q -O /usr/bin/cek-mss "myrid.digital/drg/botssh/cek-mss.sh" && chmod +x /usr/bin/cek-mss
+wget -q -O /usr/bin/delws "https://raw.githubusercontent.com/king-vpn/seller/master/botssh/delws.sh" && chmod +x /usr/bin/delws
 
 
 
-wget -q -O /usr/bin/cek-mts "myrid.digital/drg/botssh/cek-mts.sh" && chmod +x /usr/bin/cek-mts
+wget -q -O /usr/bin/delss "https://raw.githubusercontent.com/king-vpn/seller/master/botssh/delss.sh" && chmod +x /usr/bin/delss
+
+
+
+wget -q -O /usr/bin/renew-ss "https://raw.githubusercontent.com/king-vpn/seller/master/botssh/renew-ss.sh" && chmod +x /usr/bin/renew-ss
+
+
+wget -q -O /usr/bin/renewtr "https://raw.githubusercontent.com/king-vpn/seller/master/botssh/renewtr.sh" && chmod +x /usr/bin/renewtr
+
+
+wget -q -O /usr/bin/renewvless "https://raw.githubusercontent.com/king-vpn/seller/master/botssh/renewvless.sh" && chmod +x /usr/bin/renewvless
+
+
+wget -q -O /usr/bin/renewws "https://raw.githubusercontent.com/king-vpn/seller/master/botssh/renewws.sh" && chmod +x /usr/bin/renewws
+
+
+wget -q -O /usr/bin/cek-mws "https://raw.githubusercontent.com/king-vpn/seller/master/botssh/cek-mws.sh" && chmod +x /usr/bin/cek-mws
+
+wget -q -O /usr/bin/cek-mvs "https://raw.githubusercontent.com/king-vpn/seller/master/botssh/cek-mvs.sh" && chmod +x /usr/bin/cek-mvs
+
+
+wget -q -O /usr/bin/cek-mss "https://raw.githubusercontent.com/king-vpn/seller/master/botssh/cek-mss.sh" && chmod +x /usr/bin/cek-mss
+
+
+
+wget -q -O /usr/bin/cek-mts "https://raw.githubusercontent.com/king-vpn/seller/master/botssh/cek-mts.sh" && chmod +x /usr/bin/cek-mts
 
 cp /tmp/var.txt /media/cybervpn
 
