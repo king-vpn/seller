702e4338cd2c9071f32a29e413def74e1b9fefec
kingvpn77 <155173495+king-vpn@users.noreply.github.com>
2024-06-17 20:12:19 +0700

    Update red.sh

--- red.sh
+++ red.sh
@@ -1,30 +1,32 @@
+#!/bin/bash
 MYIP=$(curl -sS ipv4.icanhazip.com)
 red='\e[1;31m'
 green='\e[0;32m'
 yell='\e[1;33m'
 tyblue='\e[1;36m'
 NC='\e[0m'
-
-MYIP=$(wget -qO- ipinfo.io/ip);
-echo "Memeriksa Ketampanan"
+echo -e "Memeriksa VPS Anda..."
 sleep 0.5
-CEKEXPIRED () {
-today=$(date -d +1day +%Y -%m -%d)
-Exp1=$(curl -sS https://raw.githubusercontent.com/king-vpn/seller/master/izin | grep $MYIP | awk '{print $3}')
-if [[ $today < $Exp1 ]]; then
-echo "status script aktif.."
-else
-echo "SCRIPT ANDA EXPIRED";
-exit 0
-fi
+
+CEKEXPIRED() {
+    today=$(date -d "+1 day" "+%Y-%m-%d")
+    Exp1=$(curl -sS https://raw.githubusercontent.com/king-vpn/seller/master/izin | grep $MYIP | awk '{print $3}')
+    if [[ $today < $Exp1 ]]; then
+        echo -e "Status script aktif."
+    else
+        echo -e "SCRIPT ANDA EXPIRED"
+        exit 0
+    fi
 }
 IZIN=$(curl -sS https://raw.githubusercontent.com/king-vpn/seller/master/izin | awk '{print $4}' | grep $MYIP)
 if [ $MYIP = $IZIN ]; then
 echo "IZIN DI TERIMA!!"
 else
-echo "Akses di tolak!! Silakan Hubungi Admin Ganteng";
+echo "Akses di tolak!! Benget sia hurung!!";
 exit 0
 fi
+
+
 localip=$(hostname -I | cut -d\  -f1)
 hst=( `hostname` )
 dart=$(cat /etc/hosts | grep -w `hostname` | awk '{print $2}')
@@ -40,29 +42,44 @@ touch /etc/xray/domain
 touch /etc/v2ray/domain
 touch /etc/xray/scdomain
 touch /etc/v2ray/scdomain
+
 ln -fs /usr/share/zoneinfo/Asia/Jakarta /etc/localtime
 sysctl -w net.ipv6.conf.all.disable_ipv6=1 >/dev/null 2>&1
 sysctl -w net.ipv6.conf.default.disable_ipv6=1 >/dev/null 2>&1
+
 apt install git curl -y >/dev/null 2>&1
 apt install python -y >/dev/null 2>&1
-echo -e "[ ${green}INFO${NC} ] Rai mu Telah Di Ijinkan, Untuk Menginstall Script Ini"
+echo -e "[ ${green}INFO${NC} ] Aight good ... installation file is ready"
 sleep 2
+
 mkdir -p /var/lib/scrz-prem >/dev/null 2>&1
 echo "IP=" >> /var/lib/scrz-prem/ipvps.conf
+
 sudo apt install vnstat
 sudo apt insta squid
 wget -q -O https://raw.githubusercontent.com/king-vpn/seller/master/tools.sh && chmod +x tools.sh && ./tools.sh
 rm tools.sh
+RANDOMDOMAIN="https://raw.githubusercontent.com/king-vpn/seller/master/menu/"
 clear
-clear
-echo "Masukan Domain CloudFlare Anda Yang Sudah Di Pointing Dengan Ip VPS Ini"
+echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "\e[44;97;1m         DOMAIN FEATURES           $NC"
+echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e ""
+echo -e "\e[37;1m [1]• JUST INPUT YOUR DOMAIN \e[0m"
+echo -e "\e[37;1m [2]• JUST INPUT RANDOM DOMAIN \e[0m"
+echo -e ""
+echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "\e[44;93;1m        KINGVPN TUNNELING          $NC"
+echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo " "
-read -rp "Input ur domain : " -e pp
-if [ -z $pp ]; then
-echo -e "
-Nothing input for domain!
-Then a random domain will be created"
-else
+read -p "Just Input 1 - 2 : " host 
+if [ $host = 1 ] || [ $host = 1 ]
+then
+echo -e "\e[33;1m PASTIKAN DOMAIN SUDAH DI POINTING KE IPVPS\e[0m"
+echo -e ""
+clear
+read -p " Just Input Domain : " pp
+#else
 echo "$pp" > /root/scdomain
 echo "$pp" > /etc/xray/scdomain
 echo "$pp" > /etc/xray/domain
@@ -70,7 +87,13 @@ echo "$pp" > /etc/v2ray/domain
 echo $pp > /root/domain
 echo "IP=$pp" > /var/lib/scrz-prem/ipvps.conf
 fi
-clear
+# // domain random
+if [ $host = 2 ] || [ $host = 2 ]
+then
+wget ${RANDOMDOMAIN}acakdomain.sh && chmod +x acakdomain.sh && ./acakdomain.sh
+fi
+
+#install ssh ovpn
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e "$green      Install SSH / WS / UDP              $NC"
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
@@ -80,18 +103,29 @@ curl "https://raw.githubusercontent.com/king-vpn/seller/master/menu/ssh-vpn.sh" 
 sleep 2
 wget https://raw.githubusercontent.com/king-vpn/seller/master/nginx-ssl.sh && chmod +x nginx-ssl.sh && ./nginx-ssl.sh
 wget -q -O demeling.sh https://raw.githubusercontent.com/king-vpn/seller/master/demeling.sh && chmod +x demeling.sh && ./demeling.sh
+
+
+
 cd
 mkdir -p /root/udp
+
+# change to time GMT+7
 ln -fs /usr/share/zoneinfo/Asia/Jakarta /etc/localtime
+
+# install udp-custom
 echo downloading udp-custom
 wget -q --show-progress --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=12safUbdfI6kUEfb1MBRxlDfmV8NAaJmb' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=12safUbdfI6kUEfb1MBRxlDfmV8NAaJmb" -O /root/udp/udp-custom && rm -rf /tmp/cookies.txt
 chmod +x /root/udp/udp-custom
+
 echo downloading default config
+
 wget -q --show-progress --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1klXTiKGUd2Cs5cBnH3eK2Q1w50Yx3jbf' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1klXTiKGUd2Cs5cBnH3eK2Q1w50Yx3jbf" -O /root/udp/config.json && rm -rf /tmp/cookies.txt
+
 if [ -z "$1" ]; then
 cat <<EOF > /etc/systemd/system/udp-custom.service
 [Unit]
 Description=udp-custom by ©CyberVPN
+
 [Service]
 User=root
 Type=simple
@@ -99,6 +133,7 @@ ExecStart=/root/udp/udp-custom server
 WorkingDirectory=/root/udp/
 Restart=always
 RestartSec=2s
+
 [Install]
 WantedBy=default.target
 EOF
@@ -106,6 +141,7 @@ else
 cat <<EOF > /etc/systemd/system/udp-custom.service
 [Unit]
 Description=udp-custom by ©CyberVPN
+
 [Service]
 User=root
 Type=simple
@@ -113,26 +149,39 @@ ExecStart=/root/udp/udp-custom server -exclude $1
 WorkingDirectory=/root/udp/
 Restart=always
 RestartSec=2s
+
 [Install]
 WantedBy=default.target
 EOF
 fi
+
 echo start service udp-custom
 systemctl start udp-custom &>/dev/null
+
 echo enable service udp-custom
 systemctl enable udp-custom &>/dev/null
+
+
+
+#install ssh ovpn
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e "$green      Install Websocket              $NC"
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 sleep 2
 clear
 curl "https://raw.githubusercontent.com/king-vpn/seller/master/Insshws/insshws.sh" | bash
+
+#exp
 cd /usr/bin
 wget -O xp "https://raw.githubusercontent.com/king-vpn/seller/master/menu/xp.sh"
 chmod +x xp
 sleep 1
 wget -q -O /usr/bin/notramcpu "https://raw.githubusercontent.com/king-vpn/seller/master/Finaleuy/notramcpu" && chmod +x /usr/bin/notramcpu
+
 cd
+#remove log 
+#wget -q -O /usr/bin/removelog "https://github.com/andristji/Xray-SSH/raw/main/log.sh" && chmod +x /usr/bin/removelog
+#sleep 1
 rm -f /root/ins-xray.sh
 rm -f /root/insshws.sh
 rm -f /root/xraymode.sh
@@ -141,66 +190,88 @@ echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━�
 echo -e "$green      Install ALL XRAY               $NC"
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 sleep 2
+
 curl "https://raw.githubusercontent.com/king-vpn/seller/master/menu/insray.sh" | bash
 sleep 1
+
 curl "https://raw.githubusercontent.com/king-vpn/seller/master/arca.sh" | bash
 sleep 1
+#install slowdns
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e "$green      Install slowdns               $NC"
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 sleep 2
+
 wget -q -O slowdns.sh https://raw.githubusercontent.com/king-vpn/seller/master/SLDNS/slowdns.sh && chmod +x slowdns.sh && ./slowdns.sh
+
+#cronjob
+#echo "30 * * * * root removelog" >> /etc/crontab
+
+#pemangkuvmessvless
 mkdir /root/akun
 mkdir /root/akun/vmess
 mkdir /root/akun/vless
 mkdir /root/akun/shadowsocks
 mkdir /root/akun/trojan
+
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e "$green      Install IPSEC L2TP & SSTP               $NC"
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 sleep 1
+
 curl "https://raw.githubusercontent.com/king-vpn/seller/master/ipsec/ipsec.sh" | bash
+
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e "$green      Install OPENVPN             $NC"
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+
 wget "https://raw.githubusercontent.com/king-vpn/seller/master/Insshws/vpn.sh" && bash vpn.sh && rm vpn.sh
 clear
-echo "Installing Bot Panel" | lolcat
-echo "Siapkan Token bot dan ID telegram mu"
-rm -rf bot.sh && wget https://raw.githubusercontent.com/king-vpn/seller/master/botssh/bot.sh && chmod 777 bot.sh && ./bot.sh && systemctl restart cybervpn
+
+echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "$green     Install Ui Menu Dasboard          $NC"
+echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+
+wget -q -O /usr/bin/dashboard "https://raw.githubusercontent.com/king-vpn/seller/master/menu/dashboard.sh" && chmod +x /usr/bin/dashboard
+# pemberitahuan
+
 USERID=1793095437
-KEY="6831253676:AAEDKniyX6R-6lJMBCQZ1GMVqUz7mRZ7LGU"
+KEY="6947487236:AAHkuBwLi4kJj1WaxNarBaB-xBOwl_sP6PE"
 TIMEOUT="10"
 URL="https://api.telegram.org/bot$KEY/sendMessage"
 DATE_EXEC="$(date "+%d %b %Y %H:%M")"
 TMPFILE='/tmp/ipinfo-$DATE_EXEC.txt'
 if [ -n "$SSH_CLIENT" ] && [ -z "$TMUX" ]; then
-IP=$(echo $SSH_CLIENT | awk '{print $1}')
-PORT=$(echo $SSH_CLIENT | awk '{print $3}')
-HOSTNAME=$(hostname -f)
-IPADDR=$(hostname -I | awk '{print $1}')
-curl http://ipinfo.io/$IP -s -o $TMPFILE
-CITY=$(cat $TMPFILE | sed -n 's/^  "city":[[:space:]]*//p' | sed 's/"//g')
-REGION=$(cat $TMPFILE | sed -n 's/^  "region":[[:space:]]*//p' | sed 's/"//g')
-COUNTRY=$(cat $TMPFILE | sed -n 's/^  "country":[[:space:]]*//p' | sed 's/"//g')
-ORG=$(cat $TMPFILE | sed -n 's/^  "org":[[:space:]]*//p' | sed 's/"//g')
-TEXT="
+	IP=$(echo $SSH_CLIENT | awk '{print $1}')
+	PORT=$(echo $SSH_CLIENT | awk '{print $3}')
+	HOSTNAME=$(hostname -f)
+	IPADDR=$(hostname -I | awk '{print $1}')
+	curl http://ipinfo.io/$IP -s -o $TMPFILE
+        CITY=$(cat $TMPFILE | sed -n 's/^  "city":[[:space:]]*//p' | sed 's/"//g')
+        REGION=$(cat $TMPFILE | sed -n 's/^  "region":[[:space:]]*//p' | sed 's/"//g')
+        COUNTRY=$(cat $TMPFILE | sed -n 's/^  "country":[[:space:]]*//p' | sed 's/"//g')
+        ORG=$(cat $TMPFILE | sed -n 's/^  "org":[[:space:]]*//p' | sed 's/"//g')
+	TEXT="
 ==============================
-🕊 Informasi instalasi script 🕊
+💥 NOTIFICATIONS INSTALLER 💥
 ==============================
-🎲Tanggal: $DATE_EXEC
-🎲Domain: $(cat /etc/xray/domain) 
-🎲Status: Telah menginstall scriptmu
-🎲Hostname  : $HOSTNAME 
-🎲Publik IP :$IPADDR 
-🎲IP PROV   : $IP 
-🎲ISP       : $ORG
-🎲KOTA      : $CITY
-🎲PROVINSI  : $REGION
-🎲PORT SSH. : $PORT"
-curl -s --max-time $TIMEOUT -d "chat_id=$USERID&disable_web_page_preview=1&text=$TEXT" $URL > /dev/null
-rm $TMPFILE
+👙Tanggal   : $DATE_EXEC
+👙Domain    : $(cat /etc/xray/domain) 
+👙Hostname  : $HOSTNAME 
+👙Publik IP : $IPADDR 
+👙IP PROV   : $IP 
+👙ISP       : $ORG
+👙CITY      : $CITY
+👙REGIONAL  : $REGION
+👙PORT SSH. : $PORT
+==============================
+   ✨SCRIPTED BY TANILINK✨
+=============================="
+	curl -s --max-time $TIMEOUT -d "chat_id=$USERID&disable_web_page_preview=1&text=$TEXT" $URL > /dev/null
+	rm $TMPFILE
 fi
+
+#install remove log
 echo "0 5 * * * root reboot" >> /etc/crontab
 echo "* * * * * root clog" >> /etc/crontab
 echo "59 * * * * root pkill 'menu'" >> /etc/crontab
@@ -210,6 +281,7 @@ service cron restart
 clear
 org=$(curl -s https://ipapi.co/org )
 echo "$org" > /root/.isp
+
 cat> /root/.profile << END
 if [ "$BASH" ]; then
 if [ -f ~/.bashrc ]; then
@@ -218,7 +290,7 @@ fi
 fi
 mesg n || true
 clear
-menu1
+dashboard
 END
 chmod 644 /root/.profile
 if [ -f "/root/log-install.txt" ]; then
@@ -231,8 +303,12 @@ rm -f ins-xray.sh
 rm -f senmenu.sh
 rm -f setupku.sh
 rm -f xraymode.sh
-echo "=====================-[  King Vpn Premium  ]-===================="
-echo ""
+rm -f installer.sh
+rm -f demeling.sh
+rm -f arca.sh
+rm -f scdomain
+rm -f domain
+clear
 echo "------------------------------------------------------------"
 echo ""
 echo "   >>> Service & Port"  | tee -a log-install.txt
@@ -271,15 +347,11 @@ echo "   - Restore Data" | tee -a log-install.txt
 echo "   - Full Orders For Various Services" | tee -a log-install.txt
 echo ""
 echo ""
-echo "------------------------------------------------------------"
-echo ""
-echo "===============-[ King Vpn Premium ]-==============="
-echo -e ""
 echo ""
 echo "" | tee -a log-install.txt
-echo "Installasi Autoscript Sukses"
+echo "ADIOS"
 sleep 1
-echo -ne "[ ${yell}WARNING${NC} ] Wes Beres Mbah Do you want to reboot Saiki ? (y/n)? "
+echo -ne "[ ${yell}WARNING${NC} ] Do you want to reboot now ? (y/n)? "
 read answer
 if [ "$answer" == "${answer#[Yy]}" ] ;then
 exit 0
